6d4125cb46032cfcbdf404ee3b9feb638206aeab
khang167820 <khang167820@users.noreply.github.com>
2026-02-01 15:51:44 +0700

    Optimize website speed: gzip, caching, async fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,9 +18,16 @@
     
     <link rel="canonical" href="{{ url()->current() }}">
     
+    <!-- Performance optimizations -->
+    <link rel="dns-prefetch" href="https://fonts.googleapis.com">
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    
+    <!-- Font with font-display swap -->
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;600;700;800&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;600;700;800&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
+    <noscript><link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;600;700;800&display=swap" rel="stylesheet"></noscript>
     
     @yield('schema')
     
