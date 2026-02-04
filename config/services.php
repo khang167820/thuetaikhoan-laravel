5b243cc7cf43eb66d65ce8facb545e94e8d5566b
khang167820 <khang167820@users.noreply.github.com>
2026-02-04 16:32:19 +0700

    Add reCAPTCHA config to services.php

--- config/services.php
+++ config/services.php
@@ -35,4 +35,11 @@ return [
         ],
     ],
 
+    'recaptcha' => [
+        'site_key' => env('RECAPTCHA_SITE_KEY', ''),
+        'secret_key' => env('RECAPTCHA_SECRET_KEY', ''),
+        'enabled' => env('RECAPTCHA_ENABLED', true),
+        'score_threshold' => env('RECAPTCHA_SCORE_THRESHOLD', 0.5),
+    ],
+
 ];
